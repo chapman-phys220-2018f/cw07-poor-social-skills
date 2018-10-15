5e799d9ec69a23129397d8f2ac2983452de7fc10
Trevor Kling <kling109@mail.chapman.edu>
2018-10-15 22:22:45 +0000

    Updated Assignment

--- cw07.py
+++ cw07.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 ###
-# Name: YOUR_FULL_NAME_HERE
-# Student ID: ID_HERE
-# Email: CHAPMAN_EMAIL_HERE
+# Name: Trevor Kling
+# Student ID: 002270716
+# Email: kling109@mail.chapman.edu
 # Course: PHYS220/MATH220/CPSC220 Fall 2018
-# Assignment: HOMEWORK_OR_CLASSWORK_NUMBER
+# Assignment: CW07
 ###
 
 """Classwork 07
@@ -58,7 +58,127 @@ def gen_gaussian_array(a, b, n=1000):
             x  : [a, ..., b] Array of n equally spaced float64 between a and b
             g  : [g(a), ..., g(b)] Array of Gaussian values matched to x
     """
-    pass
+    dx = (b-a)/(n-1)
+    x = np.arange(0, n)
+    def arr_gen(i):
+        return a + i*dx
+
+    xarr = np.vectorize(arr_gen)
+    generatedArray = xarr(x)
+
+    def gauss(v):
+        return (1/math.sqrt(2*math.pi))*math.exp(-v**2/2)
+
+    garr = np.vectorize(gauss)
+    g = garr(generatedArray)
+    return((generatedArray, g))
+
+def gen_sinc_list(a, b, n=1000):
+    """
+    Generates a list based on the "sinc" function.  Based on the template for the Gaussian function above.
+
+    Args:
+      a (float): Lower bound of domain
+      b (float): Uper bound of domain
+      n (int): number of points in the domain, with a default value of 1000.
+
+    Returns:
+     (generatedList, sincList):
+       generatedList [float] : [a,...,b] A list of floats used as the inputs to the function.
+       sincList [float] : [sinc(a),...,sinc(b)] A list of floats produced by running the inputs through the sinc program.
+    """
+    dx = (b-a)/(n-1)
+    x = np.arange(0, n)
+
+    generatedList = [a + kx * dx for kx in x]
+
+    sincList = [np.divide(np.sin(t), t, out=None, where=t!=0) for t in generatedList]
+    return (generatedList, sincList)
+
+def gen_sinc_array(a, b, n=1000):
+    """
+    Generates an array based on the "sinc" function.  Based on the template for the Gaussian function above.
+
+    Args:
+      a (float): Lower bound of domain
+      b (float): Uper bound of domain
+      n (int): number of points in the domain, with a default value of 1000.
+
+    Returns:
+     (generatedArray, sincArr):
+       generatedArray [float] : [a,...,b] An array of floats used as the inputs to the function.
+       sincArr [float] : [sinc(a),...,sinc(b)] An array of floats produced by running the inputs through the sinc program.
+    """
+    dx = (b-a)/(n-1)
+    x = np.arange(0, n)
+    def arr_gen(i):
+        return a + i*dx
+
+    xarr = np.vectorize(arr_gen)
+    generatedArray = xarr(x)
+
+    sinGen = np.vectorize(np.sin)
+    sinArray = sinGen(generatedArray)
+    sincArr = np.arange(0, n)
+
+    sincArr = np.divide(sinArray, generatedArray, out=None, where=generatedArray!=0)
+
+    return (generatedArray, sincArr)
+
+def gen_sinf_list(a, b, n=1000):
+    """
+    Generates a list based on the "sinf" function.  Based on the template for the Gaussian function above.
+
+    Args:
+      a (float): Lower bound of domain
+      b (float): Uper bound of domain
+      n (int): number of points in the domain, with a default value of 1000.
+
+    Returns:
+     (generatedList, sinfList):
+       generatedList [float] : [a,...,b] A list of floats used as the inputs to the function.
+       sinfList [float] : [sinc(a),...,sinc(b)] A list of floats produced by running the inputs through the sinf program.
+    """
+    dx = (b-a)/(n-1)
+    x = np.arange(0, n)
+
+    generatedList = [a + kx * dx for kx in x]
+
+    def sinf(t):
+        return np.sin(np.divide(1, t, out=None, where=t!=0))
+
+    sinfList = [sinf(kx) for kx in generatedList]
+    return (generatedList, sinfList)
+
+def gen_sinf_array(a, b, n=1000):
+    """
+    Generates an array based on the "sinf" function.  Based on the template for the Gaussian function above.
+
+    Args:
+      a (float): Lower bound of domain
+      b (float): Uper bound of domain
+      n (int): number of points in the domain, with a default value of 1000.
+
+    Returns:
+     (generatedArray, sinfArray):
+       generatedArray [float] : [a,...,b] An array of floats used as the inputs to the function.
+       sinfArray [float] : [sinc(a),...,sinc(b)] An array of floats produced by running the inputs through the sinf program.
+    """
+    dx = (b-a)/(n-1)
+    x = np.arange(0, n)
+
+    def arr_gen(i):
+        return a + i*dx
+
+    xarr = np.vectorize(arr_gen)
+    generatedArray = xarr(x)
+
+    def sinf(t):
+        return np.sin(np.divide(1, t, out=None, where=t!=0))
+
+    sinfGen = np.vectorize(sinf)
+    sinfArray = sinfGen(generatedArray)
+    return (generatedArray, sinfArray)
 
 
 def main(a,b,n=1000):
